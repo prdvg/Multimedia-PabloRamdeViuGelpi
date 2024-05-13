5b82bbabbc322b38e4e0e193a05df06032971cc0
prdvg <pramdeviug2324@politecnics.barcelona>
2024-05-13 20:25:38 +0200

    continuando audio (he creado activity de videos)

---
+++ app/src/main/java/com/example/multimediapabloramdeviugelpi/FilmVideo.kt
@@ -0,0 +1,11 @@
+package com.example.multimediapabloramdeviugelpi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FilmVideo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_film_video)
+    }
+}
